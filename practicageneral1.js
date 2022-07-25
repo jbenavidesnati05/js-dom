bb28f3fbc9fd6161fcfe825bea68eb91bef0cc99
Jorge Osvany Benavides Nati <jbenavides@sistecredito.com>
2022-07-25 15:30:01 -0500

    subiendo agregar personas

--- practicageneral1.js
+++ practicageneral1.js
@@ -1,18 +1,60 @@
-class Persona{
-    constructor(nombre,apellido,edad){
-        this._nombre = nombre;
-        this._apellido = apellido;
-        this._edad = edad;
-    }
-    get
+class Persona {
+  static contadorperosonas = 0;
+  constructor(nombre, apellido, edad) {
+    this._idPersonas = ++Persona.contadorperosonas;
+    this._nombre = nombre;
+    this._apellido = apellido;
+    this._edad = edad;
+  }
+  get idpersona() {
+    return this._idPersonas;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
+  get edad() {
+    return this._edad;
+  }
+  set edad(edad) {
+    this._edad = edad;
+  }
 }
 
+let personas = [
 
-let nombre= document.getElementById("nombre").value;
-let apellido= document.getElementById("apellido").value;
-let edad= document.getElementById("edad").value;
+];
 
+const llenarTabla = document.querySelector("#listado");
 
-function agregar(){
+function agregarPersonas() {
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let edad = document.getElementById("edad").value;
 
+  const persona = new Persona(nombre, apellido, edad);
+  personas.push(persona);
+
+  const row = document.createElement("tr");
+  row.innerHTML = `
+            <td>${persona.idpersona}</td>
+            <td>${persona.nombre}</td> 
+            <td>${persona.apellido}</td> 
+            <td>${persona.edad}</td>
+            `;
+  llenarTabla.appendChild(row);
 }
+
+let eliminarPersona = function(){
+  this.parentNode.remoChild(this)
+}
+for(var i = 1;i<= personas.children.length -1; i++){
+personas.children[i].addEventListener("click",eliminarPersona)};
